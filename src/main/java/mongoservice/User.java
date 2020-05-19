1100992f3077ddec1686b85cb7c84f862cf77c9a
Kaushal Agarwal <kaushal.agarwal@oyorooms.com>
2020-05-19 14:38:14 +0530

    CRUD working

--- src/main/java/mongoservice/User.java
+++ src/main/java/mongoservice/User.java
@@ -34,4 +34,40 @@ public class User {
     public String getId() {
         return id;
     }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/mongoservice/UserController.java
+++ src/main/java/mongoservice/UserController.java
@@ -1,17 +1,18 @@
 package mongoservice;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.constraints.Null;
 
 @RestController
 public class UserController {
 
     @Autowired
     private UserRepository repository;
+
+    @GetMapping("/user/{number}")
+    public User getUser(@PathVariable String number) {
+        return repository.findByNumber(number);
+    }
 
     @PostMapping("/user")
     public String newUser(@RequestBody User user) {
@@ -22,5 +23,27 @@ public class UserController {
             repository.save(user);
         }
         return user.getId();
+    }
+
+    @PutMapping("/user/{id}")
+    public User updateUser(@RequestBody User user, @PathVariable String id) {
+        try {
+            repository.findById(id)
+                    .map(updatingUser -> {
+                        updatingUser.setFirstName(user.getFirstName());
+                        updatingUser.setLastName(user.getLastName());
+                        updatingUser.setNumber(user.getNumber());
+                        updatingUser.setLocation(user.getLocation());
+                        return repository.save(updatingUser);
+                    });
+        } catch (NullPointerException e) {
+            //TODO: Return for not found user
+        }
+        return user;
+    }
+
+    @DeleteMapping("/user/{id}")
+    void deleteUser(@PathVariable String id) {
+        repository.deleteById(id);
     }
 }
